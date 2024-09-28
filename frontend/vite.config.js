f6f223299852421290985a51d05dd5b68bf174c2
Rakshitpant22 <rakshitpant22@gmail.com>
2024-09-28 20:15:49 +0530

    link added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": "https://localhost:5000/",
-  //   }  
-  // } ,
+  server: {
+    proxy: {
+      "/api": "memoize-notes-app-eu2q.vercel.app"
+,
+    }  
+  } ,
   plugins: [react()],
 })
